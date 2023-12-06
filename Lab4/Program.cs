d1be104bad38b45acf5c228b6bffc9bcfada570b
threehydron <vanderfoot2016@yandex.ru>
2023-12-06 22:53:44 +0300

    FOURTH #2

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -12,9 +12,6 @@ builder.Services.AddDbContext<LrContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("LR4"));
 });
 
-//var ms = new MySection();
-
-//builder.Configuration.GetSection(MySection.Key).Bind(ms);
 
 builder.Services.AddTransient<IWordRepository, WordRepository>();
 
@@ -34,25 +31,3 @@ if (app.Environment.IsDevelopment())
 app.MapControllers();
 
 app.Run();
-        
-        
-        
-        
-        
-        /*Console.WriteLine("Выберите способ сохранения/загрузки данных:\n1. SQLite\n2. XML\n3. JSON");
-        var input = Console.ReadLine() ?? "";
-        var filename = input switch
-        {
-            "1" => "dictionary.db",
-            "2" => "dictionary.xml",
-            "3" => "dictionary.json",
-            _ => throw new Exception("Введено неверное значение.")
-        };
-
-        var database = new DatabaseConnection(filename);
-        var dictionaryDatabase = new WordDictionary(database);
-        var appController = new ApplicationController(dictionaryDatabase);
-        appController.Run();
-        database.SaveDictionary(dictionaryDatabase);
-    }
-}*/
